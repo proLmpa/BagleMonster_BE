484a337e0163d4c6497bd4586e404761e6737ccb
heeyeoul kim <kheeyeoul@gmail.com>
2023-11-06 14:27:31 +0900

    first commit

---
+++ src/main/java/com/example/baglemonster/BaglemonsterApplication.java
@@ -0,0 +1,13 @@
+package com.example.baglemonster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaglemonsterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaglemonsterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/baglemonster/BaglemonsterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.baglemonster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaglemonsterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
