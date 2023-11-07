bc3a227d54078e3b2e86874c77ff4fa7d4d790c4
heeyeoul kim <kheeyeoul@gmail.com>
2023-11-07 22:58:18 +0900

    [fix] 모든 요청에 대해 cors 접근 허용 (임시)

--- src/main/java/com/example/baglemonster/common/config/WebSecurityConfig.java
+++ src/main/java/com/example/baglemonster/common/config/WebSecurityConfig.java
@@ -17,6 +17,7 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsUtils;
 
 @Configuration
 @RequiredArgsConstructor
@@ -46,11 +47,9 @@ public class WebSecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        // CORS 설정
-        http.cors();
-
         // CSRF 설정
         http.csrf(AbstractHttpConfigurer::disable);
+        http.cors();
 
         // Session 방식 -> JWT 방식 설정 변경
         http.sessionManagement(sessionManagement ->
@@ -58,6 +57,7 @@ public class WebSecurityConfig {
 
         http.authorizeHttpRequests(authorizeHttpRequests ->
                 authorizeHttpRequests
+                        .requestMatchers(CorsUtils::isPreFlightRequest).permitAll()
                         .requestMatchers(HttpMethod.POST, "/api/users/**").permitAll()
                         .requestMatchers(HttpMethod.GET, "/api/stores/**").permitAll()
                         .requestMatchers("/swagger-ui/**", "/v3/**").permitAll()
