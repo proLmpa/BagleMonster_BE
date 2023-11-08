952dd6a29f5dc94c88c549fd4fd34848b26ee78b
heeyeoul kim <kheeyeoul@gmail.com>
2023-11-08 14:29:05 +0900

    [update] Order 전체 주석 처리. Cart 구현 완료 후 삭제 예정

--- src/main/java/com/example/baglemonster/order/repository/OrderRepository.java
+++ src/main/java/com/example/baglemonster/order/repository/OrderRepository.java
@@ -1,7 +1,7 @@
-package com.example.baglemonster.order.repository;
-
-import com.example.baglemonster.order.entity.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {
-}
+//package com.example.baglemonster.order.repository;
+//
+//import com.example.baglemonster.order.entity.Order;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//
+//public interface OrderRepository extends JpaRepository<Order, Long> {
+//}
