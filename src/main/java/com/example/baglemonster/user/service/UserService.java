767e97df90e8996b11aad41c3eebc8b305743bfb
heeyeoul kim <kheeyeoul@gmail.com>
2023-11-08 14:13:53 +0900

    [update] 회원 가입 시 사용자/가게 여부 확인 로직 추가

--- src/main/java/com/example/baglemonster/user/service/UserService.java
+++ src/main/java/com/example/baglemonster/user/service/UserService.java
@@ -21,7 +21,9 @@ public class UserService {
 
         checkDuplicateEmail(requestDto.getEmail());
 
-        userRepository.save(requestDto.toEntity(password, UserRoleEnum.USER));
+        UserRoleEnum role = requestDto.getIsStore() ? UserRoleEnum.STORE : UserRoleEnum.USER;
+
+        userRepository.save(requestDto.toEntity(password, role));
     }
 
     private void checkDuplicateEmail(String email) {
